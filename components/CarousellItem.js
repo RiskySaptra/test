fd565322163ee5945b373e74214197b97fedc373
kiki014045@gmail.com <kiki014045@gmail.com>
2022-01-05 09:13:46 +0700

    fix

--- components/CarousellItem.js
+++ components/CarousellItem.js
@@ -6,10 +6,12 @@ export default function CarousellItem({ carousellItem }) {
   return (
     <>
       <div
-        className={`flex flex-col justify-center w-[180px] h-[140px] hover:w-[190px] hover:h-[150px] m-2 hover:m-[3px] scroll-ml-2 hover:scroll-ml-[3px] snap-start rounded-lg `}
+        className={`flex flex-col justify-center items-center w-[180px] h-[140px] hover:w-[190px] hover:h-[150px] m-2 hover:m-[3px] scroll-ml-2 hover:scroll-ml-[3px] snap-start rounded-lg `}
         style={{ backgroundColor: color }}
       >
-        <Image src={image} width={40} height={40} alt={item} />
+        <div className="flex">
+          <img {...image} width={40} height={40} alt={item} />
+        </div>
         <div className="mt-3">
           <p className="text-center text-sm font-medium text-black-medium">
             {item}
